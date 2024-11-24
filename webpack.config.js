f42eaa905bffaeb69cf0f3dccd5522e74386cffa
waddles27 <127051655+waddles27@users.noreply.github.com>
2024-11-24 20:11:25 +0300

    update1

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'index.[contenthash].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+            filename: 'index.html',
+        }),
+        new FileManagerPlugin({
+            events: {
+                onStart: {
+                    delete: ['dist'],
+                },
+            },
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+    ],
+    devServer: {
+        watchFiles: path.join(__dirname, 'src'),
+        port: 9000,
+    },
+};
